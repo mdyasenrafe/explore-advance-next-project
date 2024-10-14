4aa50e7ab40514d74ee09a7fd89da604c7c38fdf
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-15 02:35:45 +0600

    loading page added

--- src/app/(withCommonLayout)/login/page.tsx
+++ src/app/(withCommonLayout)/login/page.tsx
@@ -9,6 +9,7 @@ import { loginValidationSchema } from "@/src/schemas/loginSchema";
 import { FieldValues, SubmitHandler } from "react-hook-form";
 import { useUserLogin } from "@/src/hooks/auth.hook";
 import { TLoginFormValues } from "@/src/services/AuthServices/types";
+import Loading from "@/src/components/UI/Loading";
 
 const Page = () => {
   const { mutate, isPending } = useUserLogin();
@@ -16,42 +17,45 @@ const Page = () => {
     mutate(data);
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
-        <div className="text-center">
-          <h2 className="text-2xl font-bold">Sign In</h2>
-        </div>
-        <FormWrapper
-          onSubmit={onSubmit}
-          resolver={zodResolver(loginValidationSchema)}
-        >
-          <div className="space-y-4 mt-8">
-            <div>
-              <FormInput
-                name="email"
-                label="Email address"
-                type="email"
-                placeholder="Enter your email"
-              />
-            </div>
-            <div>
-              <FormInput
-                name="password"
-                label="Password"
-                type="password"
-                placeholder="Enter your password"
-              />
-            </div>
+    <React.Fragment>
+      {isPending && <Loading />}
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded shadow-md">
+          <div className="text-center">
+            <h2 className="text-2xl font-bold">Sign In</h2>
           </div>
+          <FormWrapper
+            onSubmit={onSubmit}
+            resolver={zodResolver(loginValidationSchema)}
+          >
+            <div className="space-y-4 mt-8">
+              <div>
+                <FormInput
+                  name="email"
+                  label="Email address"
+                  type="email"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <div>
+                <FormInput
+                  name="password"
+                  label="Password"
+                  type="password"
+                  placeholder="Enter your password"
+                />
+              </div>
+            </div>
 
-          <div className="mt-6">
-            <Button fullWidth type="submit" color="primary">
-              Sign In
-            </Button>
-          </div>
-        </FormWrapper>
+            <div className="mt-6">
+              <Button fullWidth type="submit" color="primary">
+                Sign In
+              </Button>
+            </div>
+          </FormWrapper>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
