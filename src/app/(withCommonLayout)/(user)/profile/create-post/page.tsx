8c28c02a11e535d18be66b932feca03bce3ddd02
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-16 01:02:59 +0600

    added dynamic field

--- src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(withCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,7 +1,59 @@
+"use client";
+
+import FormInput from "@/src/components/form/FormInput";
+import { Button } from "@nextui-org/button";
+import { Divider } from "@nextui-org/divider";
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
+
 export default function page() {
+  const methods = useForm();
+
+  const { control, handleSubmit } = methods;
+  const { append, remove, fields } = useFieldArray({
+    control,
+    name: "questions",
+  });
+  const handleFieldAppend = () => {
+    append({ name: "questions" });
+  };
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    const postData = {
+      ...data,
+      questions: data?.questions.map(
+        (question: { value: string }) => question.value
+      ),
+    };
+    console.log(postData);
+  };
   return (
     <div>
-      <h1> Create Post Page </h1>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormInput name="Title" label="Title" />
+          <Divider className="my-5" />
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Owner verification questions</h1>
+            <Button onClick={() => handleFieldAppend()}>Append</Button>
+          </div>
+
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex items-center mt-5">
+              <FormInput name={`questions.${index}.value`} label="Question" />
+              <Button onClick={() => remove(index)}>Remove</Button>
+            </div>
+          ))}
+
+          <Divider className="my-5" />
+
+          <Button type="submit">Post</Button>
+        </form>
+      </FormProvider>
     </div>
   );
 }
