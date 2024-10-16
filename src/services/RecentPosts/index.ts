ed449de1cd3151b928fd9e228538ea6f91d198d9
Md yasen rafe <mdyasenrafe@gmail.com>
2024-10-17 00:15:09 +0600

    cache revalidating added

--- src/services/RecentPosts/index.ts
+++ src/services/RecentPosts/index.ts
@@ -2,8 +2,14 @@ import { envConfig } from "@/src/config/envConfig";
 import { delay } from "@/src/utils/delay";
 
 export const getRecentPosts = async () => {
+  const fetchOptions = {
+    next: {
+      tags: ["Posts"],
+    },
+  };
   const res = await fetch(
-    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`
+    `${envConfig.baseApi}/items?sortBy=-createdAt&limit=9`,
+    fetchOptions
   );
 
   return res.json();
